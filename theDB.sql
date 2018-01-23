fe0145adc4a375993ae65b0990be2aa4cb7cc0ab
lanchondo <30990366+lanchondo@users.noreply.github.com>
2018-01-22 20:42:12 -0600

    Create theDB.sql

---
+++ theDB.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS itemDB;
+CREATE DATABASE itemDB;
+
+USE playlistDB;
+
+CREATE TABLE bidItems(
+  id INT NOT NULL AUTO_INCREMENT,
+  Item VARCHAR(45) NULL,
+  Condition VARCHAR(45) NULL,
+  Initial_Bid INTEGER(45) NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("Watch", "New", 12);
